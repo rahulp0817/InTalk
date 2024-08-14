897fa0095263305ba1d335a788e043e210ce9068
Rahul Pradhan <rahulp0817@gmail.com>
2024-08-15 00:55:35 +0530

    files created

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+//import { View, Text } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+//import * as NavigationBar from 'expo-navigation-bar';
+import Homescreen from '../screens/Homescreen'
 
 const StackNavigator = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View>
-      <Text>StackNavigator</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor="" barStyle="dark-content" />
+      <Stack.Navigator initialRouteName='Homescreen'>
+        <Stack.Screen name="Homescreen" component={Homescreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -3,12 +3,19 @@ import React from 'react'
 
 const Homescreen = () => {
   return (
-    <View>
-      <Text>Homescreen</Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   )
 }
 
 export default Homescreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
