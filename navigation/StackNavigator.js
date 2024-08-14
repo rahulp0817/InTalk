eda807d1cf70f55494523c7b002487a1eb527380
Rahul Pradhan <rahulp0817@gmail.com>
2024-08-15 00:26:28 +0530

    files created

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StackNavigator = () => {
+  return (
+    <View>
+      <Text>StackNavigator</Text>
+    </View>
+  )
+}
+
+export default StackNavigator
