1a23fd16efd1df726fdcece138e4d2267c9e7c63
RecordBook <recordbook@MacBook-Air--Customer-Success-1.local>
2025-06-22 02:20:09 +0530

    Initilize: Installation Packages

--- config/environments.ts
+++ config/environments.ts
@@ -7,7 +7,6 @@ export interface EnvironmentConfig {
   logLevel: "debug" | "info" | "warn" | "error";
   enableAnalytics: boolean;
   enableLogging: boolean;
-  GOOGLE_MAPS_API_KEY: string;
   realmSchemaVersion: number;
   IOS_CLIENT_ID: string;
   ANDROID_CLIENT_ID: string;
@@ -20,7 +19,6 @@ export const environments: Record<Environment, EnvironmentConfig> = {
     logLevel: "debug",
     enableAnalytics: false,
     enableLogging: true,
-    GOOGLE_MAPS_API_KEY: "AIzaSyC0XnUeUCaf1XLYaVZyMusLa5kJgia6Zjw",
     realmSchemaVersion: 13,
     IOS_CLIENT_ID: "373108203712-uafj99kj247ng9uhh4gv1f28hbkj5jt3.apps.googleusercontent.com",
     ANDROID_CLIENT_ID: "373108203712-813cirr3l4n8fenh4vegu2p5e87n2lmr.apps.googleusercontent.com"
@@ -31,7 +29,6 @@ export const environments: Record<Environment, EnvironmentConfig> = {
     logLevel: "error",
     enableAnalytics: true,
     enableLogging: false,
-    GOOGLE_MAPS_API_KEY: "AIzaSyC0XnUeUCaf1XLYaVZyMusLa5kJgia6Zjw",
     realmSchemaVersion: 4,
     IOS_CLIENT_ID: "373108203712-uafj99kj247ng9uhh4gv1f28hbkj5jt3.apps.googleusercontent.com",
     ANDROID_CLIENT_ID: "373108203712-813cirr3l4n8fenh4vegu2p5e87n2lmr.apps.googleusercontent.com"
